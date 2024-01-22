1cb2f2a4200bc18d78450372c44bcf83e37f9543
Oreste <tuoreste@gmail.com>
2024-01-22 17:29:41 +0100

    initialize

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,70 +1,88 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
 # include "MLX42.h"
 # include "fcntl.h"
 # include "libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 # define MAP_W 24
 # define MAP_H 24
 # define SCREEN_W 640
 # define SCREEN_H 480
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+typedef struct s_player
+{
+	double		x;
+	double		y;
+	double		dir_x;
+	double		dir_y;
+	double		pl_x;
+	double		pl_y;
+	double		rotations;
+	double		dest_x;
+	double		dest_y;
+	double		tot_rotations;
+}				t_player;
+
 typedef struct s_ray
 {
-	double	x_dir;
-	double	y_dir;
-	double	x_plane;
-	double	y_plane;
-	double	side_dist_x;
-	double	side_dist_y;
-	double	delta_dist_x;
-	double	delta_dist_y;
-	double	camera_x;
-	double	ray_dir_x;
-	double	ray_dir_y;
-}	t_ray;
+	double		x_dir;
+	double		y_dir;
+	double		x_plane;
+	double		y_plane;
+	double		side_dist_x;
+	double		side_dist_y;
+	double		delta_dist_x;
+	double		delta_dist_y;
+	double		camera_x;
+	double		ray_dir_x;
+	double		ray_dir_y;
+}				t_ray;
 
 typedef struct s_img
 {
-	void	*img;
-	int		width;
-	int		height;
-	int		endian;
-	int		bits_per_pixel;
-	int		line_len;
-}	t_img;
+	void		*img;
+	int			width;
+	int			height;
+	int			endian;
+	int			bits_per_pixel;
+	int			line_len;
+}				t_img;
 
 typedef struct s_data
 {
-	char	*addr;
-	void	*mlx;
-	void	*win_mlx;
-	float	x0;
-	float	x1;
-	float	y0;
-	float	y1;
-}	t_data;
+	char		*addr;
+	void		*mlx;
+	void		*win_mlx;
+	float		x0;
+	float		x1;
+	float		y0;
+	float		y1;
+	t_ray		*rays;
+	t_player	*player;
+}				t_data;
 
 typedef struct s_map
 {
-	int		h;
-	int		w;
-	size_t	len_map;
-	char	**map;
-	char	*north;
-	char	*south;
-	char	*east;
-	char	*west;
-	char	*s;
-	char	*f;
-	char	*c;
-}	t_map;
-t_map	*get_map_data(char *s);
+	int			h;
+	int			w;
+	size_t		len_map;
+	char		**map;
+	char		*north;
+	char		*south;
+	char		*east;
+	char		*west;
+	char		*s;
+	char		*f;
+	char		*c;
+}				t_map;
+
+t_map			*get_map_data(char *s);
 
 #endif
--- source/main.c
+++ source/main.c
@@ -6,230 +6,11 @@
 /*   By: otuyishi <otuyishi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 22:20:31 by otuyishi          #+#    #+#             */
-/*   Updated: 2024/01/21 18:21:16 by otuyishi         ###   ########.fr       */
+/*   Updated: 2024/01/22 13:48:01 by otuyishi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-// void	map_scan(void)
-// {
-
-// }
-
-// void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-// {
-// 	char	*dst;
-
-// 	dst = data->addr + ((y * data->line_len) + (x * (data->bits_per_pixel/8)));
-// 	*(unsigned int *)dst = color;
-// }
-
-// int	main(void)
-// {
-// 	void	*mlx;
-// 	t_data	*img = NULL;
-
-// 	img = (t_data *)malloc(sizeof(t_data));
-// 	mlx = mlx_init(1920, 1080, "THIS IS CUBE3D", 100);
-// 	img->img = mlx_new_image(mlx, 1920, 1080);
-// 	int i = 0;
-// 	mlx_image_to_window(mlx, img->img, 0, 0);
-// 	while (i++ < 1000)
-// 		mlx_put_pixel(img->img, i, i, 0x00FF00FF);
-// 	mlx_loop(mlx);
-// }
-
-// int	main(int argc, char *argv[])
-// {
-// 	double	posX;
-// 	double	posY;
-// 	double	dirX;
-// 	double	dirY;
-// 	double	planeX;
-// 	double	planeY;
-// 	int		x;
-// 	double	cameraX;
-// 	double	rayDirX;
-// 	double	rayDirY;
-// 	int		mapX;
-// 	int		mapY;
-// 	double	sideDistX;
-// 	double	sideDistY;
-// 	double	deltaDistX;
-// 	double	deltaDistY;
-// 	double	perpWallDist;
-// 	int		stepX;
-// 	int		stepY;
-// 	int		hit;
-// 	int		side;
-// 	int		lineHeight;
-// 	int		drawStart;
-// 	int		drawEnd;
-// 			ColorRGB color;
-// 	double	oldDirX;
-// 	double	oldPlaneX;
-// 	double	oldDirX;
-// 	double	oldPlaneX;
-// 	void	*mlx;
-
-// 	posX = 22;
-// 	posY = 12;
-// 	dirX = -1;
-// 	dirY = 0;
-// 	planeX = 0;
-// 	planeY = 0.66;
-// 	double time = 0; //time of current frame
-//   	double oldTime = 0; //time of previous frame
-// 	mlx = mlx_init(SCREEN_W, SCREEN_H, "Raycaster", 0);
-// 	while (!done())
-// 	{
-// 		x = 0;
-// 		while (x < w)
-// 		{
-// 			cameraX = 2 * x / double (w) - 1;
-// 			rayDirX = dirX + planeX * cameraX;
-// 			rayDirY = dirY + planeY * cameraX;
-// 			deltaDistX = abs(1 / rayDirX);
-// 			deltaDistY = abs(1 / rayDirY);
-// 			if (rayDirX == 0)
-// 				deltaDistX = 1e30;
-// 			else
-// 				deltaDistX = abs(1 / rayDirX);
-// 			if (rayDirY == 0)
-// 				deltaDistY = 1e30;
-// 			else
-// 				deltaDistX = abs(1 / rayDirY);
-// 			mapX = int (posX);
-// 			mapY = int (posY);
-// 			hit = 0;
-// 			if (rayDirX < 0)
-// 			{
-// 				stepX = -1;
-// 				sideDistX = (posX - mapX) * deltaDistX;
-// 			}
-// 			else
-// 			{
-// 				stepX = 1;
-// 				sideDistX = (mapX + 1.0 - posX) * deltaDistX;
-// 			}
-// 			if (rayDirY < 0)
-// 			{
-// 				stepY = -1;
-// 				sideDistY = (posY - mapY) * deltaDistY;
-// 			}
-// 			else
-// 			{
-// 				stepY = 1;
-// 				sideDistY = (mapY + 1.0 - posY) * deltaDistY;
-// 			}
-// 			while (hit == 0)
-// 			{
-// 				if (sideDistX < sideDistY)
-// 				{
-// 					sideDistX += deltaDistX;
-// 					mapX += stepX;
-// 					side = 0;
-// 				}
-// 				else
-// 				{
-// 					sideDistY += deltaDistY;
-// 					mapY += stepY;
-// 					side = 1;
-// 				}
-// 				if (worldMap[mapX][mapY] > 0)
-// 					hit = 1;
-// 			}
-// 			if (side == 0)
-// 				perpWallDist = (sideDistX - deltaDistX);
-// 			else
-// 				perpWallDist = (sideDistY - deltaDistY);
-// 			lineHeight = (int)(h / perpWallDist);
-// 			// calculate lowest and highest pixel to fill in current stripe
-// 			drawStart = -lineHeight / 2 + h / 2;
-// 			if (drawStart < 0)
-// 				drawStart = 0;
-// 			drawEnd = lineHeight / 2 + h / 2;
-// 			if (drawEnd >= h)
-// 				drawEnd = h - 1;
-// 			switch (worldMap[mapX][mapY])
-// 			{
-// 			case 1:
-// 				color = RGB_Red;
-// 				break ; // red
-// 			case 2:
-// 				color = RGB_Green;
-// 				break ; // green
-// 			case 3:
-// 				color = RGB_Blue;
-// 				break ; // blue
-// 			case 4:
-// 				color = RGB_White;
-// 				break ; // white
-// 			default:
-// 				color = RGB_Yellow;
-// 				break ; // yellow
-// 			}
-// 			// give x and y sides different brightness
-// 			if (side == 1)
-// 				color = color / 2;
-// 			// draw the pixels of the stripe as a vertical line
-// 			verLine(x, drawStart, drawEnd, color);
-// 			oldTime = time;
-// 			time = getTicks();
-// 			double frameTime = (time - oldTime) / 1000.0;
-// 				// frameTime is the time this frame has taken, in seconds
-// 			print(1.0 / frameTime);                       // FPS counter
-// 			redraw();
-// 			cls();
-// 			// speed modifiers
-// 			double moveSpeed = frameTime * 5.0;
-// 				// the constant value is in squares/second
-// 			double rotSpeed = frameTime * 3.0;
-// 				// the constant value is in radians/second
-// 			readKeys();
-// 			// move forward if no wall in front of you
-// 			if (keyDown(SDLK_UP))
-// 			{
-// 				if (worldMap[int(posX + dirX * moveSpeed)][int(posY)] == false)
-// 					posX += dirX * moveSpeed;
-// 				if (worldMap[int(posX)][int(posY + dirY * moveSpeed)] == false)
-// 					posY += dirY * moveSpeed;
-// 			}
-// 			// move backwards if no wall behind you
-// 			if (keyDown(SDLK_DOWN))
-// 			{
-// 				if (worldMap[int(posX - dirX * moveSpeed)][int(posY)] == false)
-// 					posX -= dirX * moveSpeed;
-// 				if (worldMap[int(posX)][int(posY - dirY * moveSpeed)] == false)
-// 					posY -= dirY * moveSpeed;
-// 			}
-// 			// rotate to the right
-// 			if (keyDown(SDLK_RIGHT))
-// 			{
-// 				// both camera direction and camera plane must be rotated
-// 				oldDirX = dirX;
-// 				dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
-// 				dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
-// 				oldPlaneX = planeX;
-// 				planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
-// 				planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
-// 			}
-// 			// rotate to the left
-// 			if (keyDown(SDLK_LEFT))
-// 			{
-// 				// both camera direction and camera plane must be rotated
-// 				oldDirX = dirX;
-// 				dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
-// 				dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
-// 				oldPlaneX = planeX;
-// 				planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
-// 				planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
-// 			}
-// 			x++;
-// 		}
-// 	}
-// }
 
 int	error_exit(t_data *data, char *str)
 {
@@ -239,19 +20,22 @@ int	error_exit(t_data *data, char *str)
 	return (EXIT_FAILURE);
 }
 
-// int	initialization(t_data *data, t_img *img)
-// {
-// 	int	i;
+int	initialization(t_data *data, t_img *img)
+{
+	int	i;
 
-// 	i = 0;
-// 	data->win_mlx = mlx_image_to_window(data->mlx, img->img, 0, 0);
-// 	img->img = mlx_new_image(data->mlx, MAP_W, MAP_H);
-// }
+	i = 0;
+	data->win_mlx = mlx_image_to_window(data->mlx, img->img, SCREEN_W, SCREEN_H);
+	img->img = mlx_new_image(data->mlx, SCREEN_W, SCREEN_H);
+	data->rays = ft_calloc(sizeof(t_ray), 1);
+	data->player = ft_calloc(sizeof(t_player), 1);
+}
 
-// int	pass_parsing(t_data *data, t_map *map, char **argv)
-// {
-// 	return (1);
-// }
+int	pass_parsing(t_data *data, t_map *map, char **argv)
+{
+	return (1);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	buf[BUFFER_SIZE + 1];
